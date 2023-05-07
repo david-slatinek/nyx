ca1d893c6d7e1336c8039e7a9b349409ea264fef
david-slatinek <david.slatinek5@gmail.com>
2023-05-07 16:25:05 +0200

    Return model answer

--- dialog-model/main.py
+++ dialog-model/main.py
@@ -1,22 +1,48 @@
+from concurrent import futures
+
+import grpc
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
+import schema_pb2 as pb2
+import schema_pb2_grpc as pb2_grpc
+
+
+class DialogServiceServicer(pb2_grpc.DialogServiceServicer):
+    def __init__(self):
+        self.chat_history_ids = torch.tensor([])
+        self.bot_input_ids = torch.tensor([])
+        self.step = 0
+
+    def Dialog(self, request, context):
+        question = request.text
+        print(f"Question: {question}")
+
+        input_ids = tokenizer.encode(question + tokenizer.eos_token, return_tensors="pt")
+        self.chat_history_ids = torch.cat([self.chat_history_ids, input_ids], dim=-1)
+        self.bot_input_ids = torch.cat([self.chat_history_ids, input_ids],
+                                       dim=-1) if self.step > 0 else input_ids
+        self.chat_history_ids = model.generate(self.bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        answer = tokenizer.decode(self.chat_history_ids[:, self.bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+
+        self.step += 1
+        if self.step > 10000:
+            self.chat_history_ids = torch.tensor([])
+            self.bot_input_ids = torch.tensor([])
+            self.step = 0
+
+        return pb2.DialogResponse(answer=answer)
+
 
 if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
     model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
 
-    chat_history_ids = torch.tensor([])
-    bot_input_ids = torch.tensor([])
-    for step in range(5):
-        question = input("Question: ")
-        input_ids = tokenizer.encode(question + tokenizer.eos_token, return_tensors="pt")
+    print("Model loaded")
 
-        chat_history_ids = torch.cat([chat_history_ids, input_ids], dim=-1)
-
-        bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
-
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-        answer = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-
-        print(f"Answer: {answer}")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    pb2_grpc.add_DialogServiceServicer_to_server(DialogServiceServicer(), server)
+    server.add_insecure_port("[::]:9080")
+    server.start()
+    server.wait_for_termination()
