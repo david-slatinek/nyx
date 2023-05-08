6c7ac7a2c246508397b87393eccd5447d48ad055
david-slatinek <david.slatinek5@gmail.com>
2023-05-08 15:15:07 +0200

    Add dialog ui

--- website/src/components/Dialog.tsx
+++ website/src/components/Dialog.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, {useState} from "react";
+import axios from "axios";
 
 const Dialog = () => {
+    const [messages, setMessages] = useState([]);
+    const [inputText, setInputText] = useState('');
+
+    const handleMessageSubmit = async (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        const message = {
+            text: inputText,
+            isUser: true,
+        };
+        setMessages([...messages, message]);
+        setInputText('');
+
+        try {
+            const response = await axios.post('/api/chat', {message});
+            const data = response.data;
+            const responseMessage = {
+                text: data.message,
+                isUser: false,
+            };
+            setMessages([...messages, responseMessage]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="bg-white rounded-lg shadow-lg p-4 max-w-xs max-h-full text-center">
-                <h1 className="text-2xl font-bold">Dialog</h1>
-                <p className="text-gray-700">This is a dialog.</p>
+        <div>
+            <div className="h-screen flex flex-col justify-end px-4 py-8 space-y-4">
+                {messages.map((message, index) => (
+                    <div
+                        key={index}
+                        className={`p-4 mb-20 rounded-lg ${
+                            message.isUser ? 'bg-gray-300 self-end' : 'bg-blue-400 self-start'
+                        }`}
+                    >
+                        {message.text}
+                    </div>
+                ))}
             </div>
+            <form onSubmit={handleMessageSubmit} className="fixed bottom-0 left-0 right-0 flex px-4 py-2">
+                <input
+                    type="text"
+                    value={inputText}
+                    onChange={(event) => setInputText(event.target.value)}
+                    className="flex-1 rounded-lg p-2 mr-2 bg-gray-200"
+                />
+                <button type="submit" className="bg-blue-500 text-white rounded-lg p-2">
+                    Send
+                </button>
+            </form>
         </div>
     );
 }
