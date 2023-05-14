aae3153a7cbe40a33ea4328613ba1b83b76fc054
david-slatinek <david.slatinek5@gmail.com>
2023-05-14 17:50:12 +0200

    Add queue for email

--- recommend-api/model/recommendation.go
+++ recommend-api/model/recommendation.go
@@ -1,12 +1,5 @@
 package model
 
-type RecommendResult struct {
-	ID     string    `json:"id"`
-	Dialog string    `json:"dialog"`
-	Labels []string  `json:"labels"`
-	Scores []float32 `json:"scores"`
-}
-
 type Recommendation struct {
 	ID     string  `json:"id"`
 	Dialog string  `json:"dialog"`
