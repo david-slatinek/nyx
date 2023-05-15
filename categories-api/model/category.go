6185ffd008c934be770e959e47058bfa54f56382
david-slatinek <david.slatinek5@gmail.com>
2023-05-15 12:09:42 +0200

    Small updates for categories-api

--- categories-api/model/category.go
+++ categories-api/model/category.go
@@ -3,5 +3,5 @@ package model
 type Category struct {
 	ID            string     `json:"_id"`
 	Name          string     `json:"name" binding:"required"`
-	Subcategories []Category `json:"subcategories"`
+	Subcategories []Category `json:"subcategories,omitempty"`
 }
