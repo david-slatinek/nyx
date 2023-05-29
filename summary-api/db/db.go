6f60d3e65dca9f27d96ccca42cb0034eb4156037
david-slatinek <david.slatinek5@gmail.com>
2023-05-29 21:46:46 +0200

    Update publish.yml

--- summary-api/db/db.go
+++ summary-api/db/db.go
@@ -23,7 +23,7 @@ func NewCouchDB() (CouchDB, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	couchdb := client.DB(ctx, os.Getenv("DB_NAME"))
+	couchdb := client.DB(ctx, os.Getenv("DB_NAME_SUMMARY"))
 	if couchdb.Err() != nil {
 		return CouchDB{}, couchdb.Err()
 	}
